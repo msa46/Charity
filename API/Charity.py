1803cf963ea5e16dd4a702e2b8b4b4c90281b8f3
msa46 <mohamad.sanaye462@gmail.com>
2019-06-27 13:06:07 +0430

    Addition of basic connection to database

--- API/Charity.py
+++ API/Charity.py
@@ -1,22 +1,24 @@
 from flask_restplus import Namespace, Resource, fields
 import json
 from flask import Response
+from Utils.serializer import serializer
 import psycopg2
 conn = psycopg2.connect(database="charity", user = "admin", password = "1234", host = "127.0.0.1", port = "5432")
 cur = conn.cursor()
 
 api = Namespace('Charity',description='Charity related operations')
-
+charity_fields=['COID','Name','PostalCode','Address','PhoneNumber']
 @api.route('/')
 class Initial(Resource):
     def get(self):
         '''Return all Charities'''
         
         cur.execute('''
-            select * from charity;
+            select * from charity_organization;
         ''')
-       
-        return {"Hello": "word"}
+        records = cur.fetchall() 
+        records = serializer(charity_fields,records)
+        return records
 
 
 @api.route('/<id>')
