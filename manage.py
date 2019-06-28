fda89da701a4c0428df3e0bc78d0087204f3eeb2
mohammad <mohamad.sanaye462@zoho.com>
2019-06-28 18:52:18 +0430

    Addition of cors

--- manage.py
+++ manage.py
@@ -1,25 +1,11 @@
 from flask import Flask
 from API import api
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 app.config['SWAGGER_UI_JSONEDITOR'] = True
 api.init_app(app)
+if __name__ == '__main__':
+    app.run(debug=True)
 
-app.run(debug=True)
 
-
-# api = Api(app)
-
-# grammer=[{"word": "hello"}]
-# accepted_grammer = api.model('My_grammer',{'Word': fields.String('The word.')})
-# @api.route('/hello')
-# class HelloWorld(Resource):
-#     @api.marshal_with(accepted_grammer,envelope='the_data')
-#     def get(self):
-#         return grammer, 201
-#     @api.expect(accepted_grammer)
-#     def post(self):
-#         new_word = api.payload
-#         grammer.append(new_word)
-#         return {'result': 'Word added.'}, 201
-# if __name__ == '__main__':
-#     app.run(debug=True)
